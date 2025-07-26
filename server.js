9bf7de24a4f44ca996a283edc10982e3c53401b7
urnisha19 <urnisha09@gmail.com>
2025-07-27 02:33:57 +0600

    updated

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ admin.initializeApp({
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://admitease.vercel.app"],
+    origin: ["http://localhost:5173", "https://admit-ease.vercel.app/"],
     credentials: true,
   })
 );
